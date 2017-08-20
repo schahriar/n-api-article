62f94c9c62abcd78aca81f3df70c0ba9cd5dd388
Schahriar SaffarShargh <info@schahriar.com>
2017-08-20 15:42:15 -0700

    Fix for N-API 8.3.x

--- Getting_Started/src/module.c
+++ Getting_Started/src/module.c
@@ -8,20 +8,20 @@ napi_value MyFunction(napi_env env, napi_callback_info info) {
   status = napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
 
   if (status != napi_ok) {
-    napi_throw_error(env, "Failed to parse arguments");
+    napi_throw_error(env, NULL, "Failed to parse arguments");
   }
 
   status = napi_get_value_int32(env, argv[0], &number);
 
   if (status != napi_ok) {
-    napi_throw_error(env, "Invalid number was passed as argument");
+    napi_throw_error(env, NULL, "Invalid number was passed as argument");
   }
   napi_value myNumber;
   number = number * 2;
   status = napi_create_number(env, number, &myNumber);
 
   if (status != napi_ok) {
-    napi_throw_error(env, "Unable to create return value");
+    napi_throw_error(env, NULL, "Unable to create return value");
   }
 
   return myNumber;
